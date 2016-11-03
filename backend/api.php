891cca16b6997c1d3b11206f5f110be5e9c45c44
Olivier Laurendeau <o.laurendeau@gmail.com>
2016-11-03 17:42:12 +0100

    Add RMQ wrapper class

--- backend/api.php
+++ backend/api.php
@@ -7,22 +7,22 @@ require_once __DIR__.'/vendor/autoload.php';
 // Basic server, call function based on "method" passing "id" & "params" parameters
 $request = json_decode(file_get_contents('php://input'), true);
 try {
-    $response = $request['method']($request['id'], isset($request['params']) ? $request['params'] : null);
+    $response = $request['method']($request);
 } catch (\Exception $e) {
-    http_response_code($e->getCode());
+    http_response_code(500);
     $response = ['id' => $request['id'], 'error' => ['code' => $e->getCode(), 'message' => $e->getMessage()]];
 }
 echo json_encode($response);
 
-function createDocument($id, $params)
+function createDocument($request)
 {
-    exec('php generator.php '.$params['email'], $output, $returnVar);
+    exec('php generator.php '.$request['params']['email'], $output, $returnVar);
 
     if ($returnVar !== 0) {
         throw new \Exception("Invoice generation failed\n".$output[0], 500);
     }
 
-    $response = ['id' => $id, 'result' => 'success'];
+    $response = ['id' => $request['id'], 'result' => 'pending'];
 
     return $response;
 }
---
+++ backend/src/RabbitMQ/RabbitMQWrapper.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace RabbitMQ;
+
+use Swarrot\Broker\Message;
+use Swarrot\Broker\MessageProvider\PeclPackageMessageProvider;
+use Swarrot\Broker\MessagePublisher\PeclPackageMessagePublisher;
+
+class RabbitMQWrapper
+{
+    private $connection;
+    private $channel;
+
+    public function __construct($params = null)
+    {
+        // Create connection & channel
+        $this->connection = new \AMQPConnection($params !== null ? $params : [
+            'host' => 'rabbitmq',
+            'port' => 5672,
+            'vhost' => '/',
+            'login' => 'guest',
+            'password' => 'guest'
+        ]);
+        $this->connection->connect();
+        $this->channel = new \AMQPChannel($this->connection);
+    }
+
+    public function publish($exchangeName, Message $message, $routingKey = null)
+    {
+        $exchange = new \AMQPExchange($this->channel);
+        $exchange->setName($exchangeName);
+
+        $messagePublisher = new PeclPackageMessagePublisher($exchange);
+        $messagePublisher->publish($message, $routingKey);
+    }
+
+    public function getMessageProvider($queue)
+    {
+        return new PeclPackageMessageProvider($queue);
+    }
+}

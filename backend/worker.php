de473c01fe23a5c8ea255f2137c1bfa02d91f0bb
Olivier Laurendeau <o.laurendeau@gmail.com>
2016-11-03 21:15:12 +0100

    Make retry reality

--- backend/worker.php
+++ backend/worker.php
@@ -6,6 +6,10 @@ class JsonRpcResponseProcessor implements \Swarrot\Processor\ProcessorInterface 
 
     public function process(\Swarrot\Broker\Message $message, array $options)
     {
+        if (rand(0,3) == 0) {
+            throw new \Exception('Epic fail');
+        }
+
         $request = json_decode($message->getBody(), true);
 
         $generator = new \Generator\InvoiceGenerator();
@@ -16,11 +20,11 @@ class JsonRpcResponseProcessor implements \Swarrot\Processor\ProcessorInterface 
 $rabbitMQ = new \RabbitMQ\RabbitMQWrapper();
 $messageProvider = $rabbitMQ->getMessageProvider('queue.document');
 $stack = (new \Swarrot\Processor\Stack\Builder())
-    ->push('Swarrot\Processor\ExceptionCatcher\ExceptionCatcherProcessor')
+    ->push('Swarrot\Processor\Retry\RetryProcessor', $rabbitMQ->getMessagePublisher('retry'))
     ->push('Swarrot\Processor\Ack\AckProcessor', $messageProvider)
 ;
 
 $processor = $stack->resolve(new JsonRpcResponseProcessor());
 
 $consumer = new \Swarrot\Consumer($messageProvider, $processor);
-$consumer->consume();
+$consumer->consume(['retry_key_pattern' => 'key_%attempt%']);

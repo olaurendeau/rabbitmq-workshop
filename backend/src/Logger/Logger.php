0e21ae0d584b4de78f89c56e269fdcc49e42fb51
Olivier Laurendeau <o.laurendeau@gmail.com>
2016-11-06 12:17:47 +0100

    Improve logs

--- backend/src/Logger/Logger.php
+++ backend/src/Logger/Logger.php
@@ -22,13 +22,14 @@ class Logger
      */
     public function log($requestId, $message)
     {
-        // Log in backend/logs/app.log file
-        file_put_contents(__DIR__.'/../../logs/app.log', sprintf(
-            "[%s] %s - %s (%s)\n",
+        $log = sprintf(
+            "[%s] %s [%s] %s\n",
             (new \DateTime)->format('H:i:s'),
+            $requestId,
             $this->application,
-            $message,
-            $requestId
-        ), FILE_APPEND);
+            ucfirst($message)
+        );
+        // Log in backend/logs/app.log file
+        file_put_contents(__DIR__.'/../../logs/app.log', $log, FILE_APPEND);
     }
 }
